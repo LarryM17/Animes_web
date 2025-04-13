dc6036326cdb41b829a7780acda1fd503d3efbe0
Larry <ivanlego2012@gmail.com>
2025-04-13 18:04:56 +0200

    arreglo login

--- firebase-config.js
+++ firebase-config.js
@@ -1,13 +1,15 @@
 
-  // firebase-config.js
+// firebase-config.js
 const firebaseConfig = {
-    apiKey: "AIzaSyCoDBMuMqkdBMFPE-PTeM2gpm-suApXquE",
-    authDomain: "animes-e4f26.firebaseapp.com",
-    projectId: "animes-e4f26",
-    storageBucket: "animes-e4f26.firebasestorage.app",
-    messagingSenderId: "155204318646",
-    appId: "1:155204318646:web:2df6ba726fb0ca6115fd17"
-  };
+  apiKey: "AIzaSyCoDBMuMqkdBMFPE-PTeM2gpm-suApXquE",
+  authDomain: "animes-e4f26.firebaseapp.com",
+  projectId: "animes-e4f26",
+  storageBucket: "animes-e4f26.firebasestorage.app",
+  messagingSenderId: "155204318646",
+  appId: "1:155204318646:web:2df6ba726fb0ca6115fd17"
+};
 
 firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+
+window.db = firebase.firestore();
+window.firebase = firebase;
